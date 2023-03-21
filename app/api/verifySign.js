5b8d3608471d0638477dc9a63b8df483104de9e2
muhaiminmuhyi <muhaiminmuhyi@gmail.com>
2023-03-21 21:34:05 +0700

    Update y temen temen

--- app/api/verifySign.js
+++ app/api/verifySign.js
@@ -1,9 +1,6 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const db = require('../models/index');
 const User = require('../models').User
-const Role = require('../models').Role
-const Op = db.Sequelize.Op;
 const config = require('../config/configRoles');
 
 module.exports = {
@@ -14,29 +11,12 @@ module.exports = {
 				id: req.body.id,
 				email: req.body.email,
 				password: bcrypt.hashSync(req.body.password, 8)
-			}).then(user => {
-				Role.findAll({
-					where: {
-						name_role: {
-							[Op.or]: req.body.roles
-						}
-					}
-				}).then(roles => {
-					user.setRoles(roles).then(() => {
-						res.status(200).send({
-							auth: true,
-							id: req.body.id,
-							message: "User registered successfully!",
-							errors: null,
-						});
-					});
-				}).catch(err => {
-					res.status(500).send({
-						auth: false,
-						message: "Error",
-						errors: err
-					});
-				});
+			}).then(() => {
+				const status = {
+					status_response: 'Created',
+					status: "OK"
+				}
+				return res.status(201).send(status);
 			}).catch(err => {
 				res.status(500).send({
 					auth: false,
