eca34857d9d8a4b72be258d738eba9d6006c16eb
muhaiminmuhyi <muhaiminmuhyi@gmail.com>
2023-03-22 13:45:47 +0700

    hapus fitur dan update router kidzz

--- app/api/verifySignUp.js
+++ app/api/verifySignUp.js
@@ -37,19 +37,4 @@ module.exports = {
 			});
 		});
 	},
-
-	checkRolesExisted(req, res, next) {
-		for (let i = 0; i < req.body.roles.length; i++) {
-			if (!ROLEs.includes(req.body.roles[i].toUpperCase())) {
-				res.status(400).send({
-					auth: false,
-					id: req.body.id,
-					message: "Error",
-					errors: "Does NOT exist Role = " + req.body.roles[i]
-				});
-				return;
-			}
-		}
-		next();
-	}
 }
